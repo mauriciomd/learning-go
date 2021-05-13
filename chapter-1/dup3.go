5eabb5a64ba51ca2718fb4ba8aedb7f7776a6479
Mauricio Matter Donato <mauricio.fear@gmail.com>
2021-05-13 19:45:23 -0300

    add examples chapter 1

---
+++ chapter-1/dup3.go
@@ -0,0 +1,32 @@
+// Dup3 carrega na memória e exibe a contagem e o texto das linhas que aparecem
+// mais de uma vez na e no arquivo.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v", err)
+			continue
+		}
+
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
